d3ae5c32f0c3122d7308dba3e3d733cca2b921ac
Lee J Allen <leeallen737@gmail.com>
2023-03-06 21:22:59 +0000

    created arr for bordering countries

--- country.js
+++ country.js
@@ -3,21 +3,23 @@ let singleCountryData
 const getCountry = (alpha) => {
 
     const countryFlag = document.getElementById('country-flag')
-    
+
     fetch(`https://restcountries.com/v2/alpha/${alpha}`)
         .then(res => res.json())
         .then(data => {
             singleCountryData = data
             console.log(data.currencies[0].name)
 
-            countryFlag.innerHTML = 
-            
-            `
+            countryFlag.innerHTML =
+
+                `
                 
                 <img src="${singleCountryData.flags.svg}" class="country-flag-img"/>
 
+
+
                 <section class="section-1">
-                    <h2 class="country-h2">Some Country</h2>
+                    <h2 class="country-h2">${singleCountryData.name}</h2>
 
                     <div class="info" id="country-info-name ">
                         <h4>Native Name: </h4>
@@ -47,40 +49,82 @@ const getCountry = (alpha) => {
 
                 <section class="section-2">
 
-                        <div class="info" id="country-info-tld ">
-                            <h4>Top Level Domain: </h4>
-                            <p>${singleCountryData.topLevelDomain}</p>
-                        </div>
+                    <div class="info" id="country-info-tld ">
+                        <h4>Top Level Domain: </h4>
+                        <p>${singleCountryData.topLevelDomain}</p>
+                    </div>
                             
-                        <div class="info" id="country-info-currencies ">
-                            <h4>Currencies: </h4>
-                            <p id="currencies"></p>
-                        </div>
+                    <div class="info" id="country-info-currencies ">
+                        <h4>Currencies: </h4>
+                        <p id="currencies"></p>
+                    </div>
 
-                        <div class="info" id="country-info-languages ">
-                            <h4>Languages: </h4>
-                            <p id="languages"></p>
-                        </div>
+                    <div class="info" id="country-info-languages ">
+                        <h4>Languages: </h4>
+                        <p id="languages"></p>
+                    </div>
+                </section>
+
+                <section class="section-3" id="section-3">
+                
+                    <h3>Border Countries:</h3>
+
+                    <div class="border-countries id="border-countries"></div>
+                
                 </section>
                 
 
             `
 
             singleCountryData.currencies.forEach(currency => {
-                document.getElementById("currencies").textContent += currency.length > 1 ? `${currency.name}, ` : `${currency.name}`
-                
-             })
 
-             singleCountryData.languages.forEach(language => {
-                document.getElementById("languages").textContent += language.length > 1 ? `${language.name}, ` : `${language.name}`
-                
-             })
+                document.getElementById("currencies").textContent += currency.length > 1 ? `${currency.name}, ` : `${currency.name}`
+
+            })
+            console.log(singleCountryData)
+            //convert to function later
+            const currencies = document.getElementById("languages")
+            let currenciesText = ''
+
+            singleCountryData.currencies.forEach(currency => {
+
+                currenciesText += `${currency.name}, `
+
+            })
+
+            currenciesText = currenciesText.slice(0, currenciesText.length - 2);
+            currencies.textContent = currenciesText
+
+            //convert to function later
+            const languages = document.getElementById("languages")
+            let languagesText = ''
+
+            singleCountryData.languages.forEach(language => {
+
+                languagesText += `${language.name}, `
+
+            })
+
+            languagesText = languagesText.slice(0, languagesText.length - 2);
+            languages.textContent = languagesText
+
+            let borderCountries = ""
+
+            singleCountryData.borders.forEach(borderingCountry => {
+
+                borderCountryArr.push(borderingCountry)
+            })
+
+            console.log(borderCountryArr)
         }
         )
 
 }
 
-getCountry('BEL')
+let borderCountryArr = []
+
+getCountry('LUX')
+console.log(borderCountryArr)
 
 
 export default getCountry
